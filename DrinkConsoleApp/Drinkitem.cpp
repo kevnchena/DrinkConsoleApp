735200f6e937068bcc76f104b6a4cbb97d8c990d
KevinChena <z6256875@gmail.com>
2023-05-18 15:40:58 +0800

    飲料大哥大

---
+++ DrinkConsoleApp/Drinkitem.cpp
@@ -0,0 +1,8 @@
+#include "Drinkitem.h"
+
+Drinkitem::Drinkitem(const string& a, const string& b, int c)
+{
+	Name = a;
+	Size = b;
+	Price = c;
+}
---
+++ DrinkConsoleApp/Drinkitem.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Drinkitem
+{
+private:
+	string Name;
+	string Size;
+	int Price;
+public:
+	Drinkitem(const string&,const string&, int);
+
+};
+
