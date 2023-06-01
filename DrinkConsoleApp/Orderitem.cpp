0a91a336a2188cde7dc5c8a66d423175ec14ca79
KevinChena <z6256875@gmail.com>
2023-06-01 16:39:03 +0800

    Order

--- DrinkConsoleApp/Orderitem.cpp
+++ DrinkConsoleApp/Orderitem.cpp
@@ -13,7 +13,7 @@ void Orderitem::displayOrderitem(vector<Drinkitem>& drinks)
 {
 	Drinkitem drinkitem = drinks[itemindex - 1];
 	cout << "您所點的飲料為 : " << drinkitem.getName() << " " << drinkitem.getSize()<< " " << quantity 
-		 << "杯,每杯" << drinkitem.getPrice() << "元" << endl;
+		 << "杯,每杯" << drinkitem.getPrice() << "元,小記"<<drinkitem.getPrice()*quantity << endl;
 }
 
 int Orderitem::getindex()
